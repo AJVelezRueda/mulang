ab750bac002eb1b86ff9dadface52c3991995f89
AJVelezRueda <anavelezrueda@gmail.com>
2021-02-27 18:37:29 -0300

    add targets

--- egg/mulang/target.py
+++ egg/mulang/target.py
@@ -1,3 +1,32 @@
 class Like():
     def __init__(self, value):
         self.value = value
+    
+    def __str__(self):
+        return f"~{self.value}"
+
+class Except():
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return f"^{self.value}"
+
+
+class Named():
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return f"={self.value}"
+
+class Plain():
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+class Anyone():
+    def __str__(self):
+        return "*"
--- egg/tests/test_target.py
+++ egg/tests/test_target.py
@@ -3,3 +3,18 @@ import pytest
 
 def test_create_like_target():
     assert target.Like("Hola").value == "Hola"
+
+def test_like_to_str():
+    assert str(target.Like("Hola")) == "~Hola"
+
+def test_except_to_str():
+    assert str(target.Except("Hola")) == "^Hola"
+
+def test_named_to_str():
+    assert str(target.Named("Hola")) == "=Hola"
+
+def test_plain_to_str():
+    assert str(target.Plain("Hola")) == "Hola"
+
+def test_anyone_to_str():
+    assert str(target.Anyone()) == "*"
